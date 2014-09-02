d8d34f30a7dac22231fda22569904daeb3ea28bd
Sam Raker <sam.raker@gmail.com>
2014-09-01 21:05:40 -0400

    added twitter-related stuff to to_twitter

--- to_twitter.py
+++ to_twitter.py
@@ -8,5 +8,16 @@ from tla_ound import generator
 
 
 def do_auth():
-  return Twitter(auth=Oauth(environ.get(token), environ.get(secret),
-  environ.get(apikey), environ.get(apisecret)))
+  return Twitter(auth=Oauth(environ.get("ACCESS_TOKEN"),
+                            environ.get("ACCESS_SECRET"),
+                            environ.get("API_KEY"),
+                            environ.get("API_SECRET")))
+
+def tweet():
+  t = do_auth()
+  while True:
+    t.statuses.update(status=next(generator))
+
+
+if __name__ == "__main__":
+  tweet()
